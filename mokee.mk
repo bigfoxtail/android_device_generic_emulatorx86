f936943f52fc6036b1a9b7fb9778b5c47fa81398
bigfoxtail <bigfoxtail@hotmail.com>
2023-11-08 23:23:36 +0800

    update

--- mokee.mk
+++ mokee.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/generic/emulatorx86/emulatorx86.mk)
 
-PRODUCT_NAME := lineage_emulatorx86
+PRODUCT_NAME := mk_emulatorx86
 PRODUCT_DEVICE := emulatorx86
 PRODUCT_BRAND := LineageOS
 PRODUCT_MODEL := Full LineageOS on x86 Emulator
